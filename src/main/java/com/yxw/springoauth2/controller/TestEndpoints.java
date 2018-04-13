1d25acca7ce53ce8cef38d8ce31411a57d8c4073
yangxw <yangxw@ikangtai.com>
2018-04-13 18:50:36 +0800

    first upload

---
+++ src/main/java/com/yxw/springoauth2/controller/TestEndpoints.java
@@ -0,0 +1,32 @@
+package com.yxw.springoauth2.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: yangxw
+ * @date: Created in 2018-04-08 17:22
+ * @description:
+ **/
+@RestController
+public class TestEndpoints {
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "order id : " + id;
+    }
+
+
+}
---
+++ src/main/java/com/yxw/springoauth2/test/HM.java
@@ -0,0 +1,19 @@
+package com.yxw.springoauth2.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: yangxw
+ * @date: Created in 2018-04-12 11:53
+ * @description:
+ **/
+public class HM {
+
+    public static void main(String[] args) {
+
+
+        Map map=new HashMap(2,0.75f);
+    }
+
+}
